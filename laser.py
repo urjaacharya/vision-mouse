d3c690bbe656a5b941b07a0d187990657fc30e71
Sraddhanjali Acharya <tilaprimera@gmail.com>
2014-07-11 12:22:24 +0545

    update

--- laser.py
+++ laser.py
@@ -1,45 +1,43 @@
 from collections import Counter
 
+NEARFACTOR = 3
+
+
+#just a Container class
 class Laser(object):
-    #transition objects recorded
     x = []
     y = []
     w = []
     h = []
-    # size = []
-    area = []
-    transition = 0
     laserstate = []
-    transitionscount = 0
+    transition = 0
+    previoustate = False
 
     def __init__(self):
         pass
 
-    def add(self, x, y, w, h, state):
-        Laser.transition += 1
+    def adder(self, x, y, w, h, state):
         Laser.x.append(x)
         Laser.y.append(y)
         Laser.w.append(w)
         Laser.h.append(h)
-        # Laser.size.append((w, h))
-        #transition = counter of on/off laser objects
         Laser.laserstate.append(state)
+        if state != Laser.previoustate:
+            self.transition += 1
+        else:
+            self.transition = 0
+        Laser.previoustate = state
 
     def clear(self):
-        # when the transition trend breaks, then clear the old transition values of x, y, size
-        # and count of transitions
         Laser.x = []
         Laser.y = []
         Laser.size = []
         Laser.transition = 0
 
-    #get difference between adjacent pixels while transitiong from on to off states
     def difference(self, params):
         return [abs(params[n] - params[n - 1]) for n in range(1, len(params))]
 
-    #get the coordinate to start click or drag from
     def actioncoordinate(self):
-        #remove the values of 0 from the lists Laser.x and Laser.y
         Laser.x = filter(lambda a: a != 0, Laser.x)
         Laser.y = filter(lambda b: b != 0, Laser.y)
         frequentx = Counter(Laser.x)
@@ -48,12 +46,9 @@ class Laser(object):
         frequenty = frequenty.most_common(1)[0][0]
         return frequentx, frequenty
 
-    #blink within boundary is a click else drag
     def boundaryevaluate(self):
-        #get difference of consecutive x and y values
-        #if more near values then click else drag
-        nearcount = 0 #within the NEARFACTOR limit
-        farcount = 0 #outside of the NEARFACTOR limit
+        nearcount = 0
+        farcount = 0
         localx = Laser.x
         localy = Laser.y
         xdiff = self.difference(localx)
@@ -63,7 +58,6 @@ class Laser(object):
                 nearcount += 1
             else:
                 farcount += 1
-        #nearcount means within boundary as per NEARFACTOR value
         if nearcount > farcount:
             return True
         else:
