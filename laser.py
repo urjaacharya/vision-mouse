5ccac55b052e3cbb7beb849d9eb5398730431777
Sraddhanjali Acharya <tilaprimera@gmail.com>
2014-07-14 18:24:20 +0545

    state diagram implementation

--- laser.py
+++ laser.py
@@ -1,9 +1,9 @@
+import socket
 from collections import Counter
 
-# NEARFACTOR = 3
-CONTINUALBEAMTHRESH = 10
+THRESHHOLD = 4
 
-#just a Container class
+
 class Laser(object):
     x = []
     y = []
@@ -11,82 +11,111 @@ class Laser(object):
     h = []
     laserstate = []
     previoustate = False
-    #click variables
-    click = False
-    dragstart = False
-    decision = ''
-    count = -1
-    transition = 0
+    client = None
 
-    def __init__(self):
-        pass
+    def __init__(self, clientsocket):
+        self.client = clientsocket
 
-    def adder(self, x, y, w, h, state):
-        #appending is LAST IN operation
+        #states
+        self.continualzero = None
+        self.continualone = None
+        self.toggleto0 = None
+        self.toggleto1 = None
+        self.one = 0
+        self.zero = 0
+
+    def container(self, x, y, w, h, state):
         self.x.append(x)
         self.y.append(y)
         self.w.append(w)
         self.h.append(h)
         self.laserstate.append(state)
-        #get 15 recent samples of x and y ---> [inclusive: exclusive] slicing operation
-        samplex = self.x[9:26]
-        sampley = self.y[9:26]
-        samplestate = self.laserstate[9:26]
 
-        #transition block
-        if self.previoustate != state:
-            self.previoustate = state
-            flag = self.continualbeam(samplestate, 0)
-            if flag:
-                self.count += 1
-                if self.count == 0:
-                    print "laser seen for the first time"
-                    self.count = -1
-            else:
-                self.transition += 1
-                flag1 = self.checkclick(samplex, sampley)
-                # flag2 = self.checkdrag(samplex, sampley)
-                if flag1:
-                    print "action click"
-                    self.click = True
-                    if self.x[-1] == 0:
-                        decision = 'c' + str(self.x[-2]) + ';' + str(self.x[-2])
-                    else:
-                        decision = 'c' + str(self.x[-1]) + ';' + str(self.x[-1])
-                    return decision
-
-        #continual 1 or 0's block
-        else:
-            self.previoustate = state
-            if self.dragstart or self.click:
-                flag = self.continualbeam(samplestate, 1)
-                if flag:
-                    self.dragstart = False
-                    self.click = False
-            #if laserstate is On, then only move
-            if state:
-                decision = 'm' + str(self.x[-1]) + str(self.y[-1])
-                return decision
-
+        #first we clear the buffer to size 10 before making any analysis
         self.bufferclear()
 
-    def checkclick(self, samplex, sampley):
-        if self.transition == 1:
-            if abs(samplex[-1] - samplex[-2]) <= 4:
-                return True
+        #no states have been initialized to
+        if self.continualzero is None and self.continualone is None and self.toggleto0 is None and self.toggleto1 is None:
+            continualzero = self.continualbeam(self.laserstate, 0)
+            if not continualzero:
+                self.continualzero = False
+                continualone = self.continualbeam(self.laserstate, 1)
+                if not continualone:
+                    self.continualone = False
+                    toggleto0 = self.toggle(state)
+                    if toggleto0:
+                        self.toggleto0 = toggleto0
+                        self.zero += 1
+                    else:
+                        self.toggleto1 = True
+                        self.one += 1
+                else:
+                    self.continualone = True
             else:
-                return False
+                self.continualzero = True
 
-    # def checkdrag(self, samplex, sampley):
+        if self.continualzero:
+            if state == 1:
+                self.toggleto1 = True
+                self.continualzero = False
+                self.continualone = False
+                self.toggleto0 = False
+            else:
+                print "we send nothing"
 
-    #check for continuous 0's or 1's within the CONTINUALBEAMTHRESH
+        if self.continualone:
+            if state == 0:
+                self.toggleto0 = True
+                self.continualone = False
+                self.continualzero = False
+                self.toggleto1 = False
+                self.client.send('md' + str(self.x[-2]) + ';' + str(self.y[-2]) + '\0')
+            else:
+                self.client.send('m' + str(self.x[-1]) + ';' + str(self.y[-1]) + '\0')
+
+        if self.toggleto0:
+            if state == 1:
+                self.toggleto1 = True
+                self.toggleto0 = False
+                self.continualzero = False
+                self.continualone = False
+                self.client.send('md' + str(self.x[-1]) + ';' + str(self.y[-1]) + '\0')
+            else:
+                self.zero += 1
+                if self.zero > THRESHHOLD:
+                    self.toggleto0 = False
+                    self.continualzero = True
+                    print "went outside"
+
+        if self.toggleto1:
+            if state == 0:
+                self.toggleto0 = True
+                self.toggleto1 = False
+                self.continualone = False
+                self.continualzero = False
+                self.client.send('md' + str(self.x[-2]) + ';' + str(self.y[-2]) + '\0')
+            else:
+                self.one += 1
+                if self.one > THRESHHOLD:
+                    self.toggleto1 = False
+                    self.continualone = True
+                self.client.send('m' + str(self.x[-1]) + ';' + str(self.y[-1]) + '\0')
+
+    def toggle(self, state):
+        if self.previoustate != state and state == 0:
+            print "1 to 0"
+            return True
+        elif self.previoustate != state and state == 1:
+            print "0 to 1"
+            return False
+
+    # check for continuous 0's or 1's within the CONTINUALBEAMTHRESH
     def continualbeam(self, samplestate, factor):
         continual = 0
         for state in samplestate:
             if state == factor:
                 continual += 1
                 if continual >= CONTINUALBEAMTHRESH:
-                    self.transition = 0
                     return True
             else:
                 return False
@@ -100,39 +129,3 @@ class Laser(object):
                 self.w.pop(0)
                 self.h.pop(0)
                 self.laserstate.pop(0)
-
-    def clear(self):
-        self.x = []
-        self.y = []
-        self.w = []
-        self.h = []
-        self.laserstate = []
-
-    # def difference(self, params):
-    #     return [abs(params[n] - params[n - 1]) for n in range(1, len(params))]
-    #
-    # def actioncoordinate(self):
-    #     Laser.x = filter(lambda a: a != 0, Laser.x)
-    #     Laser.y = filter(lambda b: b != 0, Laser.y)
-    #     frequentx = Counter(Laser.x)
-    #     frequentx = frequentx.most_common(1)[0][0]
-    #     frequenty = Counter(Laser.y)
-    #     frequenty = frequenty.most_common(1)[0][0]
-    #     return frequentx, frequenty
-    #
-    # def boundaryevaluate(self):
-    #     nearcount = 0
-    #     farcount = 0
-    #     localx = Laser.x
-    #     localy = Laser.y
-    #     xdiff = self.difference(localx)
-    #     ydiff = self.difference(localy)
-    #     for x, y in zip(xdiff, ydiff):
-    #         if x < NEARFACTOR and y < NEARFACTOR:
-    #             nearcount += 1
-    #         else:
-    #             farcount += 1
-    #     if nearcount > farcount:
-    #         return True
-    #     else:
-    #         return False
